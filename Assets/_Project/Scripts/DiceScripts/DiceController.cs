77de133db3cfc6ea390402bea317361906ea4a02
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-18 00:51:22 +0300

    dummy hinge joint tested

--- Assets/_Project/Scripts/DiceScripts/DiceController.cs
+++ Assets/_Project/Scripts/DiceScripts/DiceController.cs
@@ -1,45 +1,75 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using _Project.Scripts.LeonsExtensions.Clamp;
 using DG.Tweening;
-using Unity.VisualScripting;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using UnityEngine.Serialization;
 
 namespace _Project.Scripts.DiceScripts
 {
     public class DiceController : MonoBehaviour
     {
-        private readonly Vector3 _startPos = new Vector3(10, .3f, -7.5f);
+        private readonly Vector3 _startPos = new Vector3(10, 1f, -7.5f);
+        
+        [SerializeField] private Rigidbody _diceRigidbody;
 
-        [SerializeField] private ClampVal _randomX;
-        [SerializeField] private ClampVal _randomZ;
+
+      [SerializeField] private List<GameObject> _dices;
+
+
+        private Vector3 _lastVelocity;
+        private Vector3 _lastAngularVelocity;
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                RandomJump();
+                RollDice(10f, 15f);
             }
         }
 
-        private void RandomJump()
+        private void RollDice(float throwForce, float rollForce)
         {
-            transform.DOKill();
-            var startPos = new Vector3(Random.Range(-1f, 1f), 0, 0) + _startPos;
+            _dices[0].SetActive(false);
+            _dices[1].SetActive(false);
+            _diceRigidbody.isKinematic = false;
+            StopAllCoroutines();
+            _diceRigidbody.velocity = Vector3.zero;
+            _diceRigidbody.angularVelocity = Vector3.zero;
 
-            transform.position = startPos;
-            var randomRotation = new Vector3(Random.Range(-360f, 360f), Random.Range(-360f, 360f),
-                Random.Range(-360f, 360f));
-            transform.rotation = Quaternion.Euler(randomRotation);
-            var randomEndTarget = new Vector3(_randomX.RandomValue, 0.15f, _randomZ.RandomValue);
-            var pathList = LeonsExtensions.LeonsMath.ParabolaPoints(transform.position, randomEndTarget, 5f);
+            transform.position = _startPos;
+            transform.rotation = Random.rotation;
 
-            transform.DOPath(pathList.ToArray(), .5f);
 
-            transform.DORotate(Vector3.zero, Random.Range(.5f,.75f));
+            var targetPos = new Vector3(Random.Range(-1f, 1f), Random.Range(4f, 5f), Random.Range(-1f, 1f));
+            var randomVariance = Random.Range(-1f, 1f);
+            _diceRigidbody.AddForce((targetPos - transform.position).normalized * (throwForce + randomVariance),
+                ForceMode.Impulse);
 
+            var rndX = Random.Range(-1f, 1f);
+            var rndY = Random.Range(-1f, 1f);
+            var rndZ = Random.Range(-1f, 1f);
+            _diceRigidbody.AddTorque(new Vector3(rndX, rndY, rndZ) * (rollForce * randomVariance), ForceMode.Impulse);
+
+            StartCoroutine(Cheat());
+        }
+
+        private IEnumerator Cheat()
+        {
+            yield return new WaitUntil((() => (Vector3.Distance(_diceRigidbody.velocity, Vector3.zero) <= .1f) || Vector3.Distance(_diceRigidbody.angularVelocity,Vector3.zero)<=.1f));
+            var rnd = Random.Range(0, 1f);
+            if (rnd < .5f)
+            {
+                Debug.LogError("Dice : 5");
+                _dices[0].SetActive(false);
+                _dices[1].SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Dice : 3");
+                _dices[0].SetActive(true);
+                _dices[1].SetActive(false);
+            }
         }
     }
 }
