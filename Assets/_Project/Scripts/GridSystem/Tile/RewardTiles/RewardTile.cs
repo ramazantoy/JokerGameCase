848dd2650a97e3c3042f67c563de03d4c55308ba
Ramazan  TOY <ramcotoy06@gmail.com>
2024-05-22 21:59:41 +0300

    prefabs created material changes

--- Assets/_Project/Scripts/GridSystem/Tile/RewardTiles/RewardTile.cs
+++ Assets/_Project/Scripts/GridSystem/Tile/RewardTiles/RewardTile.cs
@@ -6,13 +6,6 @@ namespace _Project.Scripts.GridSystem.Tile.RewardTiles
 {
     public abstract class RewardTile : RoadTileBase
     {
-        public Color TileColor;
-
-        public void Awake()
-        {
-            transform.GetComponent<MeshRenderer>().material.DOColor(TileColor, 0f);
-        }
-
         public abstract override void GiveRewards();
 
     }
